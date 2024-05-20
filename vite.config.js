e9170d1f03a2e746cca241e95a14e7c7daead8c1
Ankit Prajapathi A <ankitprajapathi25@gmail.com>
2024-05-20 20:35:57 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "",
+  plugins:[react()],
   root: path.join(__dirname, "src"),
   build: {
     outDir: path.join(__dirname, "dist"),
